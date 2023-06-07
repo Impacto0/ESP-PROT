082ec4ebd3e7e3b7f2ca65939fd325462639cc96
Impacto0 <37021709+Impacto0@users.noreply.github.com>
2023-06-07 23:16:41 +0200

    Podstawowe pliki biblioteki

---
+++ include/ESPProt.h
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+
+// EPM - ESP Protocol Message
+struct EPM {
+    unsigned int ID : 7; // 7 bitów na ID (0-127)
+    unsigned int Type : 1; // 1 bit na typ (0 - zapytanie, 1 - odpowiedź)
+    unsigned int Data : 24; // 24 bity na dane (0-16777215)
+};
+
+class ESPProt
+{
+public:
+    // metoda Init() jest wywoływana w setup() i inicjalizuje komunikację (używana po stronie klienta i serwera)
+    void Init();
+
+    // metoda RequestSensorData() używana jest po stronie serwera
+    void RequestSensorData(int type, int sensorID);
+
+    // metoda SendSensorData() używana jest po stronie klienta
+    void SendSensorData(int type, int sensorID, int data);
+};
---
+++ src/ESPProt.cpp
@@ -0,0 +1,16 @@
+#include <ESPProt.h>
+
+void ESPProt::Init()
+{
+    // ...
+}
+
+void ESPProt::RequestSensorData(int type, int sensorID)
+{
+    // ...
+}
+
+void ESPProt::SendSensorData(int type, int sensorID, int data)
+{
+    // ...
+}

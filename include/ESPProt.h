7ba6800bafae7c5b31bf64f3358d43a2ffb97ebf
Impacto0 <37021709+Impacto0@users.noreply.github.com>
2023-06-08 00:06:26 +0200

    Inicjalizacja klienta oraz serwera

--- include/ESPProt.h
+++ include/ESPProt.h
@@ -1,21 +1,43 @@
 #include <Arduino.h>
 
 // EPM - ESP Protocol Message
-struct EPM {
-    unsigned int ID : 7; // 7 bitów na ID (0-127)
+typedef struct EPM {
+    unsigned int ClientID : 4; // 4 bity na ID klienta (0-15)
     unsigned int Type : 1; // 1 bit na typ (0 - zapytanie, 1 - odpowiedź)
     unsigned int Data : 24; // 24 bity na dane (0-16777215)
-};
+} EPM;
 
 class ESPProt
 {
 public:
     // metoda Init() jest wywoływana w setup() i inicjalizuje komunikację (używana po stronie klienta i serwera)
+    // isServer - czy urządzenie jest serwerem (true) czy klientem (false)
+    // metoda inicjalizuje WiFi i ESP-NOW
     void Init(bool isServer);
 
     // metoda RequestSensorData() używana jest po stronie serwera
+    // wysyła zapytanie o dane do klienta
     void RequestSensorData(int type, int sensorID);
 
     // metoda SendSensorData() używana jest po stronie klienta
+    // wysyła dane do serwera
     void SendSensorData(int type, int sensorID, int data);
+
+    // metoda AddClient() używana jest po stronie serwera
+    // dodaje odbiorcę, do którego będą wysyłane dane
+    void AddClient(uint8_t* mac);
+
+    // metoda SetServer() używana jest po stronie klienta
+    // ustawia nadawce danych
+    void SetServer(uint8_t* mac);
+
+    void OnDataRecv(EPM message);
+
+    bool IsServer = false;
+
+private:
+    uint8_t clients[10][6];
+    uint8_t server[6];
+
+    int clientCount = 0;
 };
--- src/ESPProt.cpp
+++ src/ESPProt.cpp
@@ -1,16 +1,82 @@
 #include <ESPProt.h>
+#include <esp_now.h>
 
-void ESPProt::Init()
+#ifdef ESP32
+  #include <WiFi.h>
+#else
+  #include <ESP8266WiFi.h>
+#endif
+
+esp_now_peer_info_t _peerInfo;
+
+void OnDataRecvCallback(const uint8_t *mac, const uint8_t *data, int len);
+
+void ESPProt::Init(bool isServer)
 {
-    // ...
+	IsServer = isServer;
+
+	Serial.begin(115200);
+
+	WiFi.mode(WIFI_STA);
+
+	Serial.printf("MAC: %s\n", WiFi.macAddress().c_str());
+
+	if(esp_now_init() != ESP_OK){
+		Serial.println("Error initializing ESP-NOW");
+		return;
+	}
+
+	_peerInfo.channel = 0;
+	_peerInfo.encrypt = false;
+
+	if(IsServer){
+		Serial.println("Setting up server...");
+
+		// Dodawanie klientów
+		for(int i = 0; i < clientCount && i < 10; i++){
+			memcpy(_peerInfo.peer_addr, clients[i], 6);
+			if (esp_now_add_peer(&_peerInfo) != ESP_OK){
+				Serial.printf("Failed to add client with MAC: %02X:%02X:%02X:%02X:%02X:%02X\n",
+				_peerInfo.peer_addr[0], _peerInfo.peer_addr[1], _peerInfo.peer_addr[2],
+				_peerInfo.peer_addr[3], _peerInfo.peer_addr[4], _peerInfo.peer_addr[5]);
+				continue;
+			}
+		}
+
+		// Rejestrowanie callbacka
+		esp_now_register_recv_cb(OnDataRecvCallback);
+
+		Serial.println("Server set up!");
+	}
+	else{
+		Serial.println("Setting up client...");
+
+		// Dodawanie serwera
+		memcpy(_peerInfo.peer_addr, server, 6);
+		if (esp_now_add_peer(&_peerInfo) != ESP_OK){
+			Serial.printf("Failed to add server with MAC: %02X:%02X:%02X:%02X:%02X:%02X\n",
+			_peerInfo.peer_addr[0], _peerInfo.peer_addr[1], _peerInfo.peer_addr[2],
+			_peerInfo.peer_addr[3], _peerInfo.peer_addr[4], _peerInfo.peer_addr[5]);
+		}
+
+		// Rejestrowanie callbacka
+		esp_now_register_recv_cb(OnDataRecvCallback);
+
+		Serial.println("Client set up!");
+	}
 }
 
 void ESPProt::RequestSensorData(int type, int sensorID)
 {
-    // ...
+	// ...
 }
 
 void ESPProt::SendSensorData(int type, int sensorID, int data)
 {
-    // ...
+	// ...
+}
+
+void OnDataRecvCallback(const uint8_t *mac, const uint8_t *data, int len)
+{
+	// ...
 }
